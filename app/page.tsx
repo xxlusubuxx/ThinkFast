bb13b99088804d113bf0f0425c9a53814a7b80a4
Chris Garciae <hungdoan160796@gmail.com>
2024-11-03 14:08:01 +0700

    adding test page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -22,6 +23,9 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly. really?</li>
         </ol>
+        <Link href="/app/test"> {/* Specify the path to test.tsx */}
+        <a>Go to Test Page</a> {/* This will be your button */}
+        </Link>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
---
+++ app/test.tsx
@@ -0,0 +1,7 @@
+export default function testing() {
+    return (
+        <div>
+            <h1>Hello! jut testing to see if this shit runs properly lol</h1>
+        </div>
+    )
+}

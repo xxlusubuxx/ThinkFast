4c3b6faa05465f3aea6b1f0f5110b7c5400a1a2a
Chris Garciae <hungdoan160796@gmail.com>
2024-11-04 23:04:03 +0700

    git commit -m 'just do it already lol'

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
           src="/next.svg"
           alt="Next.js logo"
           width={180}
-          height={38}
+          height={32}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
